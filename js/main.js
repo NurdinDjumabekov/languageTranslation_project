baf0dc68e042aa6dc003606bc8bf60f25bdf7b43
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-06-27 23:09:41 +0600

    changeAllPages

--- js/main.js
+++ js/main.js
@@ -1,36 +1,79 @@
 let bool = true;
-const btn_ru = document.querySelector("#btn_ru");
-const btn_en = document.querySelector("#btn_en");
-btn_ru.style.color = "red";
-btn_ru.style.boxShadow = "0px 2px 30px rgba(0, 0, 0, 0.1)";
+const btn_ru = document.querySelectorAll(".btn_ru");
+const btn_en = document.querySelectorAll(".btn_en");
+const btn_kg = document.querySelectorAll(".btn_kg");
+btn_ru[0].style.color = "blue";
+btn_ru[0].style.boxShadow = "0px 2px 30px rgba(0, 0, 0, 0.2)";
+btn_ru[1].style.color = "blue";
+btn_ru[1].style.boxShadow = "0px 2px 30px rgba(0, 0, 0, 0.2)";
 
-btn_ru.addEventListener("click", () => {
-  btn_ru.style.color = "red";
-  btn_ru.style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.1)";
-  btn_en.style.color = "";
-  btn_en.style.boxShadow = "";
+btn_ru[1].addEventListener("click", () => {
+  btn_ru[1].style.color = "blue";
+  btn_ru[1].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_en[1].style.color = "";
+  btn_en[1].style.boxShadow = "";
+  btn_kg[1].style.color = "";
+  btn_kg[1].style.boxShadow = "";
 });
 //////////////////////////////////////////
-btn_en.addEventListener("click", () => {
-  btn_en.style.color = "red";
-  btn_en.style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.1)";
-  btn_ru.style.color = "";
-  btn_ru.style.boxShadow = "";
+btn_en[1].addEventListener("click", () => {
+  btn_en[1].style.color = "blue";
+  btn_en[1].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_ru[1].style.color = "";
+  btn_ru[1].style.boxShadow = "";
+  btn_kg[1].style.color = "";
+  btn_kg[1].style.boxShadow = "";
 });
-const btn_html = document.querySelector(".btn_html");
-const btn_price = document.querySelector(".btn_price");
-const btn_order = document.querySelector(".btn_order");
-const btn_aboutUs = document.querySelector(".btn_aboutUs");
-if (window.location.href === "http://127.0.0.1:5500/price.html") {
-  btn_price.style.color = "red";
-} else if (window.location.href === "http://127.0.0.1:5500/order.html") {
-  btn_order.style.color = "red";
-} else if (window.location.href === "http://127.0.0.1:5500/abourUs.html") {
-  btn_aboutUs.style.color = "red";
-} else if (window.location.href === "http://127.0.0.1:5500/index.html") {
-  btn_html.style.color = "red";
+//////////////////////////////////////////
+btn_kg[1].addEventListener("click", () => {
+  btn_kg[1].style.color = "blue";
+  btn_kg[1].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_en[1].style.color = "";
+  btn_en[1].style.boxShadow = "";
+  btn_ru[1].style.color = "";
+  btn_ru[1].style.boxShadow = "";
+});
+btn_ru[0].addEventListener("click", () => {
+  btn_ru[0].style.color = "blue";
+  btn_ru[0].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_en[0].style.color = "";
+  btn_en[0].style.boxShadow = "";
+  btn_kg[0].style.color = "";
+  btn_kg[0].style.boxShadow = "";
+});
+//////////////////////////////////////////
+btn_en[0].addEventListener("click", () => {
+  [0];
+  btn_en[0].style.color = "blue";
+  btn_en[0].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_ru[0].style.color = "";
+  btn_ru[0].style.boxShadow = "";
+  btn_kg[0].style.color = "";
+  btn_kg[0].style.boxShadow = "";
+});
+//////////////////////////////////////////
+btn_kg[0].addEventListener("click", () => {
+  btn_kg[0].style.color = "blue";
+  btn_kg[0].style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.2)";
+  btn_en[0].style.color = "";
+  btn_en[0].style.boxShadow = "";
+  btn_ru[0].style.color = "";
+  btn_ru[0].style.boxShadow = "";
+});
+const btn_html = document.querySelectorAll(".btn_html");
+const btn_price = document.querySelectorAll(".btn_price");
+const btn_order = document.querySelectorAll(".btn_order");
+const btn_aboutUs = document.querySelectorAll(".btn_aboutUs");
+if (window.location.href.includes("price.html")) {
+  btn_price.forEach((btn) => (btn.style.color = "blue"));
+} else if (window.location.href.includes("order.html")) {
+  btn_order.forEach((btn) => (btn.style.color = "blue"));
+} else if (window.location.href.includes("aboutUs.html")) {
+  btn_aboutUs.forEach((btn) => (btn.style.color = "blue"));
+} else if (window.location.href.includes("index.html")) {
+  btn_html.forEach((btn) => (btn.style.color = "blue"));
 } else {
-  btn_html.style.color = "red";
+  btn_html.forEach((btn) => (btn.style.color = "blue"));
 }
 ////////////////////////////////////////////
 const btn_nav = document.querySelector("#btn_nav");
